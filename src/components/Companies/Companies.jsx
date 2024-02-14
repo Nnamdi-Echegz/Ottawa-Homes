b17c55d08f1c417056c6ffc6736a997265e476fe
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2024-02-13 21:43:32 -0500

    logosection complete

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Companies.css";
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+      <img src="./cadconsulting.png" alt="associate company logo" />
+        <img src="./realty.png" alt="associate company logo" />
+        <img src="./landhome.png" alt="associate company logo" />
+        <img src="./equinix.png" alt="associate company logo" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
